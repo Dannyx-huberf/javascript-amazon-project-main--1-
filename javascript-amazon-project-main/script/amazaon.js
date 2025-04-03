eb82f1cb36e60b158d07537790c9e9757d17ed06
Daniel <maddisonjames891@gmail.com>
2025-04-03 06:56:47 +0100

    used import on products.js

--- javascript-amazon-project-main/script/amazaon.js
+++ javascript-amazon-project-main/script/amazaon.js
@@ -1,4 +1,5 @@
 import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 // looping through our product object in the products array
 let productHtml = '';
 products.forEach((product)=>{
