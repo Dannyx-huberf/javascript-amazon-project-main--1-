9a9968ae806ec1efdc8ff63cfca4275660caa594
Daniel <maddisonjames891@gmail.com>
2025-04-05 02:38:30 +0100

    save and generate html of deliveyOption

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -4,11 +4,13 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
    cart =  [
   {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity:2
+    quantity:2,
+    deliveryOptionId:'1'
   },
   {
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1
+    quantity:1,
+    deliveryOptionId:'2'
   }
 ];
 }
@@ -28,7 +30,8 @@ export function addtoCart(productId,productContainer){
     // Add new item with selected quantity
     cart.push({
       productId: productId,
-      quantity: selectedQuantity
+      quantity: selectedQuantity,
+      deliveryOptionId: '1' // Default delivery option ID
     });
   } else {
     // Update existing item's quantity (add selected quantity)
